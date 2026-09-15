326fb10073b2596ee0737a29fc434fe0d1191ff9
Ankita Mandal <cutelyankita@gmail.com>
2026-09-15 22:56:44 +0530

    Add leaf nodes program

---
+++ 02_Tree/Q5_Leaf_Nodes.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def count_leaf(root):
+    if root is None:
+        return 0
+
+    if root.left is None and root.right is None:
+        return 1
+
+    return count_leaf(root.left) + count_leaf(root.right)
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print("Number of leaf nodes:", count_leaf(root))
